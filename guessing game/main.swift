00a0e9cb7dbaed3f2c19eb2399766b326c26c634
Christian Lamb <christianlamb@MacBook-Pro-52.local>
2018-08-23 08:51:55 -0500

    Base game

---
+++ guessing game/main.swift
@@ -0,0 +1,71 @@
+//
+//  main.swift
+//  guessing game
+//
+//  Created by Christian Lamb on 8/23/18.
+//  Copyright © 2018 Christian Lamb. All rights reserved.
+//
+
+import Foundation
+
+var playingGame = true
+
+
+print("Welcome to The guessing game!!!")
+print("You have 5 guesses to get the answer right")
+repeat {
+    var stillPlaying = "Y"
+    var guessesLeft = 5
+    
+    let randomNumber = Int(arc4random_uniform(101))
+    
+    
+    
+    repeat{
+        
+        print("Please guess a number between 1 - 100.")
+        var userGuess = Int(readLine()!)
+        
+        while userGuess == nil {
+            print("Not a number betweeen 1 - 100 ")
+            print("Please guess a number between 0 - 100.")
+            userGuess = Int(readLine()!)
+        }
+        if guessesLeft == 0 {
+            print(" game over ")
+        }
+        
+        if userGuess! > randomNumber && userGuess != randomNumber {
+            print("The Number is lower.")
+        }
+        if userGuess! < randomNumber && userGuess != randomNumber {
+            print("The number is higher")
+        }
+        if randomNumber == userGuess{
+            print("You guessed the number.")
+            guessesLeft = 0
+        }
+        if userGuess != randomNumber{
+            guessesLeft -= 1
+            print("Guesses left \(guessesLeft)")
+        }
+        if guessesLeft == 0 && userGuess != randomNumber{
+            print("You lose")
+        }
+        
+        if userGuess == randomNumber || guessesLeft == 0 {
+            print("The correct answer was \(randomNumber)")
+            print("would you like to play agian Y/N.")
+            stillPlaying  = readLine()!.uppercased()
+        }
+        if stillPlaying == "N"{
+            playingGame = false
+        }
+        
+    }while guessesLeft > 0
+    
+}while playingGame == true
+
+print("Thanks for playing.")
+
+
